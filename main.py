f8f0854e9fbcad7570075af14c5c2a4a752c2fdb
Christian Villegas <99997132+Chrivill02@users.noreply.github.com>
2023-02-14 09:50:06 -0600

    Ultimo commit
    Abri el archivo.py en pycharm y me creó una carpeta idea, pero ya realicé lo de .gitignore
    
    Co-Authored-By: Miguel Matul <miguemat4@gmail.com>

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Christian Villegas, 1592623")
+import os
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+	print("La carpeta ya existe")
+else:
+	os.mkdir(mi_ubicacion + "\\modulos")
+	archivo = open('./modulos/prueba.txt','w')
+	archivo.write('Hola mundo')
+	archivo.close()	
